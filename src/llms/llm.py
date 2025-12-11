8338323f7c2f60592e9f661c28cd5022ca9ecc76
hhhhsc <1710496817@qq.com>
2025-12-11 17:15:58 +0800

    feature: update llm cache

--- src/llms/llm.py
+++ src/llms/llm.py
@@ -135,12 +135,8 @@ def get_llm_by_type(llm_type: LLMType) -> BaseChatModel:
     """
     Get LLM instance by type. Returns cached instance if available.
     """
-    if llm_type in _llm_cache:
-        return _llm_cache[llm_type]
-
     conf = load_yaml_config(_get_config_file_path())
     llm = _create_llm_use_conf(llm_type, conf)
-    _llm_cache[llm_type] = llm
     return llm
 
 
